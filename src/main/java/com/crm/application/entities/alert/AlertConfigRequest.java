ba488afaefa9b4138258afdfe6a3ef3303d2e9b2
El <irina.elina.r@gmail.com>
2025-03-23 18:49:43 +0300

    [feat] config alert percentage

---
+++ src/main/java/com/crm/application/entities/alert/AlertConfigRequest.java
@@ -0,0 +1,11 @@
+package com.crm.application.entities.alert;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AlertConfigRequest {
+    String id;
+    double percentage;
+}
